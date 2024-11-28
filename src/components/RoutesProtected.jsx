9b7f660b1bd19a60726948a549022df646c1cb09
Expedito de Paulo <irospidermanstronger@gmail.com>
2024-11-28 07:13:33 -0300

    commits

--- src/components/RoutesProtected.jsx
+++ src/components/RoutesProtected.jsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-
 const RouteProtected = ({ children }) => {
   const Navigate = useNavigate();
-  const { signed } = useAuth();
-  console.log("Estado de autenticação no RouteProtected:", signed);
+  const { signed, loading } = useAuth(); // Adicione `loading` ao hook de autenticação
+
+  if (loading) {
+    return <p>Carregando...</p>; // Exiba algo enquanto valida a autenticação
+  }
 
   if (!signed) {
     console.log("Acesso negado. Redirecionando para login.");
